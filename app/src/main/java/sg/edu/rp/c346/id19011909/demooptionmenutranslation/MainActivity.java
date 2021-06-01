4f79426388d10b2ffe75fa94056e0bd6c93f89ae
Shawn <19011909@myrp.edu.sg>
2021-06-01 21:09:00 +0800

    Initial commit {Completed}

---
+++ app/src/main/java/sg/edu/rp/c346/id19011909/demooptionmenutranslation/MainActivity.java
@@ -0,0 +1,64 @@
+package sg.edu.rp.c346.id19011909.demooptionmenutranslation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Creating Variable,
+    TextView tvTranslatedText;
+
+    TextView tvTranslatedText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Linking Variable.
+        tvTranslatedText = findViewById(R.id.textViewTranslatedText);
+
+        tvTranslatedText2 = findViewById(R.id.textViewTranslatedText2);
+
+        //Setting Action,
+        registerForContextMenu(tvTranslatedText);
+        registerForContextMenu(tvTranslatedText2);
+    }
+
+    //Displaying Dropdown List,
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Translating Language,
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello");
+            tvTranslatedText2.setText("Bye");
+            return true;
+        } else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            tvTranslatedText2.setText("Addio");
+            return true;
+        } else {
+            tvTranslatedText.setText("Error translation");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
